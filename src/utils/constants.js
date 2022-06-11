0bbd1166fda2ac5bb8014ababc393daec1d39db4
Acetylcholine007 <66874501+Acetylcholine007@users.noreply.github.com>
2022-06-11 15:54:47 +0800

    Added GoogleMaps component

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const API_URL = "http://192.168.1.18:8000";
-
+const MAP_API = "AIzaSyBCIJfldUcipchlxKIfyinkd1JAbpVf60I";
 const LS_USER_DATA = "userData";
 const TOKEN_EXPIRATION = new Date(new Date().getTime() + 1000 * 60 * 60);
 const APP_DEBUG = true;
